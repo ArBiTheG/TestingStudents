62a5143627c9654b8845152ce918bf5957af2c1c
unknown <don.grabovsky@yandex.ru>
2022-10-03 21:36:36 +0300

    Create Programs

---
+++ TestingStudentsData/AppContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace TestingStudentsData
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Tables.Chapter> Chapters { get; set; }
+        public AppContext() : base("DefaultConnection") { }
+    }
+}
---
+++ TestingStudentsData/Table/Chapter.cs
@@ -0,0 +1,12 @@
+﻿namespace TestingStudentsData.Tables
+{
+    public class Chapter
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public byte[] picture { get; set; }
+        public int parent_id { get; set; }
+        public bool active { get; set; }
+    }
+}
